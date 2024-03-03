c14a6e3b0fe77a1377f91214c70c0a9b3ff99fe4
root <root@YJS>
2024-03-04 06:22:29 +0900

    calender-app

---
+++ src/main/java/com/example/calenderapp/CalenderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calenderapp/CalenderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calenderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
