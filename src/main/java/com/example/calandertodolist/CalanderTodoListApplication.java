6db67f914ed8900a3eb33fb81d77c5db53f057df
keeheung <xl7923@naver.com>
2024-03-04 17:13:34 +0900

    Toy project todo list start

---
+++ src/main/java/com/example/calandertodolist/CalanderTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.calandertodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalanderTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalanderTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calandertodolist/CalanderTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calandertodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalanderTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
